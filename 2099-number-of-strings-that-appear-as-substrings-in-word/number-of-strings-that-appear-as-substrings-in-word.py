3037252310723bedd9b3355802026c8ae28a9aab
Ananya <157976759+ananyadivakar@users.noreply.github.com>
2026-06-29 20:01:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.82%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        count = 0
+
+        for p in patterns:
+            if p in word:
+                count += 1
+
+        return count
