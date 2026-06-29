32cbdded58059af987d281e7aebdd5e090293a4b
Ananya <157976759+ananyadivakar@users.noreply.github.com>
2026-06-29 20:13:06 +0530

    Time: 40 ms (43.15%) | Memory: 23.8 MB (81.74%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        last_index = {}
+
+        for i, num in enumerate(nums):
+            if num in last_index and i - last_index[num] <= k:
+                return True
+
+            last_index[num] = i
+
+        return False
