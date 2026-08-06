3d47e849f91cbea52d49a7c5d41bd2e8685de021
Ananya <157976759+ananyadivakar@users.noreply.github.com>
2026-08-06 11:49:29 +0530

    Time: 42 ms (35.64%) | Memory: 13.3 MB (61.47%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canJump(self, nums):
+        reach = 0
+
+        for i, jump in enumerate(nums):
+            if i > reach:
+                return False
+            reach = max(reach, i + jump)
+
+        return True
